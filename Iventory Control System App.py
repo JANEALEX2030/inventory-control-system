35bef6b1f025830e86e7071f0fce29b562cd927a
jane <fostinogeorge@gmail.com>
2025-05-03 22:09:38 -0800

    login
    this helps to display the login button ,if the passwors are incorrect the user can not login it will show an error,hence tightening the security

--- Iventory Control System App.py
+++ Iventory Control System App.py
@@ -184,4 +184,43 @@ def open_inventory_window():
                     restock_win.destroy()
                     load_items()
                 except Exception as e:
-                    messagebox.showerror("Restock Error", s
+                    messagebox.showerror("Restock Error",
+                                         init_db()
+
+                    login_window = tk.Tk()
+                    login_window.title("Login")
+                    login_window.geometry("300x250")
+                    login_window.configure(bg="#e6f0ff")
+
+                    tk.Label(login_window, text="Username", bg="#e6f0ff").pack(pady=5)
+                    username_entry = tk.Entry(login_window)
+                    username_entry.pack()
+
+                    tk.Label(login_window, text="Password", bg="#e6f0ff").pack(pady=5)
+                    password_entry = tk.Entry(login_window, show="*")
+                    password_entry.pack()
+
+                    tk.Button(login_window, text="Login", command=login, bg="#4da6ff", fg="white").pack(pady=10)
+                    tk.Button(login_window, text="Sign Up", command=open_signup_window, bg="#4da6ff", fg="white").pack()
+
+                    login_window.mainloop()
+                    # -------------------- APP START --------------------
+                    init_db()
+
+                    login_window = tk.Tk()
+                    login_window.title("Login")
+                    login_window.geometry("300x250")
+                    login_window.configure(bg="#e6f0ff")
+
+                    tk.Label(login_window, text="Username", bg="#e6f0ff").pack(pady=5)
+                    username_entry = tk.Entry(login_window)
+                    username_entry.pack()
+
+                    tk.Label(login_window, text="Password", bg="#e6f0ff").pack(pady=5)
+                    password_entry = tk.Entry(login_window, show="*")
+                    password_entry.pack()
+
+                    tk.Button(login_window, text="Login", command=login, bg="#4da6ff", fg="white").pack(pady=10)
+                    tk.Button(login_window, text="Sign Up", command=open_signup_window, bg="#4da6ff", fg="white").pack()
+
+                    login_window.mainloop()
